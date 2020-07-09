98ec2f1f9a3a48e431c8fde9cfd1241a9840d95c
COllignonB <collignon.baptiste.pro@gmail.com>
2020-07-09 14:18:24 +0200

    check if letter exist and update the underscore chain

--- js/main.js
+++ js/main.js
@@ -9,25 +9,57 @@ const wordList = [
 
 // Function Definition
 
+// Generate a random number and choice a word
 function generateComputerChoice(){
     let randomIndex = Math.floor(Math.random() * Math.floor(4));
     let computerChoice = wordList[randomIndex];
     return computerChoice;
 }
 
+// tranform a word into an array 
 function transformChain(word){
     let chain = word.split('');
     return chain;    
 
 }
+// create a String of "_ " based on the length of the computerChoice
+function chainTounderscore(word){
+    let under = "";
+    for (let i = 0; i < word.length; i ++){
+        under += "_ ";
+    }
+    return under;
+}
+
+function checkUserInput(letter){
+    while(letter.length !== 1){
+        letter = prompt("Veuillez rentrer une seule lettre");
+    }
+    return letter;
+}
+
+function isInChain(computerChoice, userChoice){
+    let chainArray = transformChain(computerChoice);
+    for (let i = 0; i < chainArray.length; i ++){
+        if (chainArray[i] !== userChoice){
+            chainArray[i] = "_ ";
+        }
+    }
+    console.log(chainArray);
+    return chainArray;
+}
+
 function main(){
 
     alert("Bonjour, vous allez jouer au jeu du pendu");
 
     let computerChoice = generateComputerChoice();
     let chain = transformChain(computerChoice);
-
-    let userChoice = prompt("Choisissez une lettre : ");
+    console.log(computerChoice);
+    let underChain = chainTounderscore(computerChoice);
+    let userChoice = checkUserInput(prompt("Le mot a trouver : "+underChain +"\nChoisissez une lettre : "));
+    underChain = isInChain(computerChoice, userChoice);
+    console.log(userChoice);
 
 }
 
