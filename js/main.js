fd1dc91aba875baa57e8bea6ad41c5d4922f2927
COllignonB <collignon.baptiste.pro@gmail.com>
2020-07-09 17:29:51 +0200

    need to fix checks after first one

--- js/main.js
+++ js/main.js
@@ -18,16 +18,19 @@ function generateComputerChoice(){
 
 // tranform a word into an array 
 function transformChain(word){
-    let chain = word.split('');
-    return chain;    
+    let table = word.split('');
+    console.log(" taille de la chaine "+table.length);
+    console.log(`${word} devient ${table}`);
+    return table;    
 
 }
 // create a String of "_ " based on the length of the computerChoice
 function chainTounderscore(word){
-    let under = "";
+    let under = [];
     for (let i = 0; i < word.length; i ++){
-        under += "_ ";
+        under[i] = "_";
     }
+    console.log(under);
     return under;
 }
 
@@ -38,28 +41,49 @@ function checkUserInput(letter){
     return letter;
 }
 
-function isInChain(computerChoice, userChoice){
-    let chainArray = transformChain(computerChoice);
-    for (let i = 0; i < chainArray.length; i ++){
-        if (chainArray[i] !== userChoice){
-            chainArray[i] = "_ ";
+function isInChain(table, userChoice){
+    let result = table;
+    console.log(table);
+    console.log("avant la boucle");
+    for (let i = 0; i < result.length; i ++){
+        if (result[i] === "_" && result[i] !== userChoice){
+            result[i] = userChoice;
         }
     }
-    console.log(chainArray);
-    return chainArray;
+    console.log("bite");
+    console.log(table);
+    return result.join('');
 }
+
+function isUnderLeft (underChain){
+    let underCount = 0;
+    for (const letter of underChain) {
+        if (letter === "_"){
+            underCount ++;
+            console.log(underCount);
+        }      
+    }
+    return underCount === 0 ? true : false;
+}
+
 
 function main(){
 
     alert("Bonjour, vous allez jouer au jeu du pendu");
 
     let computerChoice = generateComputerChoice();
-    let chain = transformChain(computerChoice);
-    console.log(computerChoice);
+    const table = transformChain(computerChoice);
+    console.log(table);
     let underChain = chainTounderscore(computerChoice);
-    let userChoice = checkUserInput(prompt("Le mot a trouver : "+underChain +"\nChoisissez une lettre : "));
-    underChain = isInChain(computerChoice, userChoice);
-    console.log(userChoice);
+    let userChoice;
+    // isUnderLeft(underChain);
+    while(underChain !== computerChoice){
+        userChoice = checkUserInput(prompt("Le mot a trouver : "+underChain +"\nChoisissez une lettre : "));
+        //console.log(`user input = ${userChoice}`);
+        underChain = isInChain(table, userChoice);
+        console.log(underChain);
+        console.log("c'est table : " + table);
+    }
 
 }
 
